db5def65038fec782ff3db9dad4ade61073e457e
NITIN Prajapati <91796080+nitinprajapati01@users.noreply.github.com>
2024-05-18 04:33:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
+  name                = "test-atlantis"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
