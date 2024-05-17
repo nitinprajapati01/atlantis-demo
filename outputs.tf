a5a3ee2713abb9911a5ce485b8830b9e17b4ef54
NITIN Prajapati <91796080+nitinprajapati01@users.noreply.github.com>
2024-05-18 04:57:22 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "resource_group_name" {
   value = azurerm_resource_group.example.name
 }
 
-output "virtual_network_name" {
-  value = azurerm_virtual_network.example.name
-}
+# output "virtual_network_name" {
+# value = azurerm_virtual_network.example.name
+# }
